7e556bf4bd914e9ebc459a11834e6e2826ac336e
Gopi <GopiR5@yahoo.com>
2024-06-18 20:17:12 +0530

    ip changed

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import { ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
-  const url="https://cpbackend-eoae.onrender.com/";
+  const url="https://cpbackend-eoae.onrender.com";
   return (
     <div>
       <ToastContainer />
